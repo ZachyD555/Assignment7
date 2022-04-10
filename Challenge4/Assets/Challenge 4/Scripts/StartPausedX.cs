4246b428018545adf9b07e2eba60aecfc6afc5c5
ZachyD555 <zdaly22@gmail.com>
2022-04-10 13:46:49 -0500

    almost done

---
+++ Challenge4/Assets/Challenge 4/Scripts/StartPausedX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartPausedX : MonoBehaviour
+{
+    public Text rules;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 1;
+            rules.gameObject.SetActive(false);
+        }
+    }
+}
